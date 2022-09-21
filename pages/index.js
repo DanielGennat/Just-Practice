fe9ce8ee9ece80ab0bfbb48c95ad92b483f5ab0e
DanielGennat <daniel.gennat@freenet.de>
2022-09-21 14:42:37 +0200

    add 5 minutes countdown

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import styled from 'styled-components';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <CountdownCircleTimer
+        isPlaying
+        duration={300}
+        colors={[
+          ['#004777', 0.33],
+          ['#F7B801', 0.33],
+          ['#A30000', 0.33],
+        ]}
+      >
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    </div>
+  );
 }
