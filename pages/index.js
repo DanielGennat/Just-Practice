5cc54020b671ad6546c77aee64d09918c0cfd805
DanielGennat <daniel.gennat@freenet.de>
2022-09-21 16:06:41 +0200

    add conditional renderTime with minutes and seconds

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,39 @@ import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import styled from 'styled-components';
 
 export default function Home() {
+  const renderTime = ({ remainingTime }) => {
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+
+    if (seconds < 60 && minutes < 1) {
+      return (
+        <div>
+          <div>{seconds}</div>
+        </div>
+      );
+    } else if (seconds < 10) {
+      return (
+        <div>
+          <div>
+            {minutes}:0{seconds}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>
+            {minutes}:{seconds}
+          </div>
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
       <CountdownCircleTimer isPlaying duration={300} colors={'#abc'}>
-        {({ remainingTime }) => remainingTime}
+        {renderTime}
       </CountdownCircleTimer>
     </div>
   );
