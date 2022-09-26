501ee425f2db69b7e5a1c4e6808c914035794569
DanielGennat <daniel.gennat@freenet.de>
2022-09-26 22:37:49 +0200

    fix testing

--- pages/index.test.js
+++ pages/index.test.js
@@ -1,21 +1,15 @@
 import { renderTime } from './index.js';
 
-function renderTime(callback, remainingTime) {
-  if (remainingTime === 0) {
-    callback(playNotificationSound());
-  }
-}
-
 describe('renderTime', () => {
   test('timer has expired', () => {
     const playNotificationSound = jest.fn();
-    Home(playNotificationSound, 0);
+    renderTime(0, playNotificationSound);
     expect(playNotificationSound).toHaveBeenCalled();
   });
 
   test('timer has not expired yet', () => {
     const playNotificationSound = jest.fn();
-    Home(playNotificationSound, 1);
+    renderTime(1, playNotificationSound);
     expect(playNotificationSound).not.toHaveBeenCalled();
   });
 });
