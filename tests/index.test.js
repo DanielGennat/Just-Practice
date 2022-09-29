be6edb1990a477fe1621b15c95bb592d1d9e9568
DanielGennat <daniel.gennat@freenet.de>
2022-09-29 12:41:03 +0200

    wip test duration changed

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
 import { renderTime } from '../pages/index.js';
+import { setUpNextInterval } from '../pages/index.js';
 
 describe('renderTime', () => {
   test('timer has expired', () => {
@@ -11,5 +12,23 @@ describe('renderTime', () => {
     const playNotificationSound = jest.fn();
     renderTime(1, playNotificationSound);
     expect(playNotificationSound).not.toHaveBeenCalled();
+  });
+});
+
+// describe('duration', () => {
+//   test('changed to next item of timerChain', () => {
+//     const timerChain = [30, 60];
+//     const [timerPointer, setTimerPointer] = useState(0);
+//     let duration = timerChain[timerPointer];
+//     setUpNextInterval(timerChain, timerPointer, setTimerPointer);
+//     expect(duration).toBe(60);
+//   });
+// });
+
+describe('timperPointer', () => {
+  test('changed to next item of timerChain', () => {
+    let timerPointer = 0;
+    setUpNextInterval(timerPointer);
+    expect(timerPointer).toBe(1);
   });
 });
