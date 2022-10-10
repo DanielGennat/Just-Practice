bee41a164141e772991c9b1130ba5860f7cf970c
DanielGennat <daniel.gennat@freenet.de>
2022-10-10 17:14:56 +0200

    add test for the intervals update

--- tests/handleAdd.test.js
+++ tests/handleAdd.test.js
@@ -1,27 +1,12 @@
-import handleAdd from '../components/EditFormComponents/EditTimerChainForm';
+import { handleAdd } from '../components/EditFormComponents/EditTimerChainForm';
 
 describe('newTimerChain', () => {
   test('has new timer with 5 minutes', () => {
     let newTimerChain = [{ id: 1, minutes: 1, seconds: 10 }];
     const setNewTimerChain = jest.fn();
-    const addId = newTimerChain.length + 1;
-    handleAdd(newTimerChain, setNewTimerChain);
     const newTimerChainAfterHandleAdd = [
       { id: 1, minutes: 1, seconds: 10 },
-      { id: addId, minutes: 5, seconds: 0 },
-    ];
-    expect(newTimerChain).toMatchObject(newTimerChainAfterHandleAdd);
-  });
-});
-
-describe('newTimerChain', () => {
-  test('has new timer with 5 minutes', () => {
-    let newTimerChain = [{ id: 1, minutes: 1, seconds: 10 }];
-    const setNewTimerChain = jest.fn();
-    const addId = newTimerChain.length + 1;
-    const newTimerChainAfterHandleAdd = [
-      { id: 1, minutes: 1, seconds: 10 },
-      { id: addId, minutes: 5, seconds: 0 },
+      { id: 2, minutes: 5, seconds: 0 },
     ];
     handleAdd(newTimerChain, setNewTimerChain);
     expect(setNewTimerChain).toHaveBeenLastCalledWith(
