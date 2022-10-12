6e602649be6c40ce477eee8a76e747def9cec520
DanielGennat <daniel.gennat@freenet.de>
2022-10-12 12:14:23 +0200

    add name key-value pairs

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,10 @@ export function handleSubmit(
 
 export default function Home() {
   const initialChain = [
-    { id: 1, minutes: 0, seconds: 30 },
-    { id: 2, minutes: 1, seconds: 10 },
-    { id: 3, minutes: 1, seconds: 2 },
-    { id: 4, minutes: 1, seconds: 40 },
+    { id: 1, minutes: 0, seconds: 30, name: 'Exercise 1' },
+    { id: 2, minutes: 1, seconds: 10, name: 'practice sth. else' },
+    { id: 3, minutes: 1, seconds: 2, name: 'a minor arpeggio' },
+    { id: 4, minutes: 1, seconds: 40, name: 'my favorite song' },
   ];
   const [timerChain, setTimerChain] = useLocalStorage(
     '_timerChainStorage',
