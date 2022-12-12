a270f772e97f17f924ae06b342c2dae8749864a1
X4 <daniel.gennat@freenet.de>
2022-12-12 11:31:19 +0100

    refactor FormExerciseList

--- pages/edit.js
+++ pages/edit.js
@@ -8,7 +8,7 @@ import useLocalStorage from '../hooks/useLocalStorage';
 import { useRouter } from 'next/router';
 import BackgroundImage from '../components/BackgroundImage';
 import pedalsImg from '../public/pedals.jpg';
-import changeTimerChain from '../components/EditFormComponents/ChangeTimerChain';
+import FormExerciseList from '../components/EditFormComponents/FormExerciseList';
 
 export function handleAdd(newTimerChain, setNewTimerChain) {
   const addId = newTimerChain.length + 1;
@@ -38,11 +38,6 @@ export default function EditTimerChainForm({
     '_timerChainStorage',
     timerChain
   );
-
-  const timeOptions = [];
-  for (let i = 0; i <= 59; i++) {
-    timeOptions.push(i);
-  }
 
   const router = useRouter();
 
@@ -87,76 +82,7 @@ export default function EditTimerChainForm({
           handleSubmit(event);
         }}
       >
-        <FormList>
-          {newTimerChain.map((newTimer) => (
-            <Li key={newTimer.id}>
-              <InputAndLabelWrapper>
-                <ExerciseName
-                  id="exerciseName"
-                  type="text"
-                  value={newTimer.exerciseName}
-                  placeholder={`Exercise ${newTimer.id}`}
-                  onChange={(event) =>
-                    changeTimerChain(
-                      event,
-                      newTimer.id,
-                      newTimerChain,
-                      setNewTimerChain
-                    )
-                  }
-                />
-                <Label htmlFor="exerciseName"> Exercise</Label>
-              </InputAndLabelWrapper>
-              <InputAndLabelWrapper>
-                <Select
-                  id="minutes"
-                  value={newTimer.minutes}
-                  onChange={(event) =>
-                    changeTimerChain(
-                      event,
-                      newTimer.id,
-                      newTimerChain,
-                      setNewTimerChain
-                    )
-                  }
-                >
-                  {timeOptions.map((option) => {
-                    return (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    );
-                  })}
-                </Select>
-                <Label htmlFor="minutes">min.</Label>
-              </InputAndLabelWrapper>
-              <span>:</span>
-              <InputAndLabelWrapper>
-                <Select
-                  id="seconds"
-                  value={newTimer.seconds}
-                  onChange={(event) =>
-                    changeTimerChain(
-                      event,
-                      newTimer.id,
-                      newTimerChain,
-                      setNewTimerChain
-                    )
-                  }
-                >
-                  {timeOptions.map((option) => {
-                    return (
-                      <option key={option} value={option}>
-                        {option < 10 ? `0${option}` : option}
-                      </option>
-                    );
-                  })}
-                </Select>
-                <Label htmlFor="seconds"> sec.</Label>
-              </InputAndLabelWrapper>
-            </Li>
-          ))}
-        </FormList>
+        <FormExerciseList newTimerChain={newTimerChain} setNewTimerChain={setNewTimerChain} />
         <ButtonWrapper>
           <AddButton
             type="button"
@@ -205,57 +131,6 @@ const TopGap = styled.div`
 
 const Form = styled.form`
   padding-bottom: 50px;
-`;
-
-const FormList = styled.ol`
-  background-color: rgba(52, 108, 61, 0.7);
-  margin: 5px 10vw;
-  border-radius: 5px;
-  padding: 15px 0;
-  list-style-type: none;
-  counter-reset: li;
-`;
-
-const Li = styled.li`
-  display: flex;
-  justify-content: center;
-
-  &::before {
-    counter-increment: li;
-    content: counter(li, decimal-leading-zero);
-    color: #caf6ff;
-    margin: 0 5px 0 0;
-  }
-`;
-
-const InputAndLabelWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ExerciseName = styled.input`
-  background-color: #edffdf;
-  margin: 0 1px;
-  border-radius: 5px;
-  border: none;
-  width: 30vw;
-  max-width: 340px;
-`;
-
-const Select = styled.select`
-  text-align: right;
-  background-color: #edffdf;
-  width: 50px;
-  margin: 0 1px;
-  border-radius: 5px;
-  border: none;
-`;
-
-const Label = styled.label`
-  color: #caf6ff;
-  font-weight: 300;
-  font-size: 0.8rem;
 `;
 
 const ButtonWrapper = styled.div`
